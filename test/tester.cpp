4cf69c2aa1d17acc1e6f05dd8f05fc24c4e5d7c1
ylian121 <yongyanliang05@gmail.com>
2024-05-30 23:28:29 -0700

    added more test for each

--- test/tester.cpp
+++ test/tester.cpp
@@ -2,7 +2,25 @@
 
 #include "../lib/testsubject.h"
 
-TEST(MathTest, Square) {
+TEST(MathTest, Square1) {
     ASSERT_EQ(TestSubject::square(2), 4);
+}
+TEST(MathTest, Square2) {
     ASSERT_EQ(TestSubject::square(3), 9);
+}
+
+TEST(MathTest, Square3) {
+    ASSERT_EQ(TestSubject::square(4), 16);
+}
+
+TEST(MathTest, isEven1) {
+    ASSERT_EQ(TestSubject::isEven(4), true);
+}
+
+TEST(MathTest, isEven2) {
+    ASSERT_EQ(TestSubject::isEven(3), false);
+}
+
+TEST(MathTest, isEven3) {
+    ASSERT_EQ(TestSubject::isEven(0), true);
 }
